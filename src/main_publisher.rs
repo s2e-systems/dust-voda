cc596637f81f2bc0559f09bf4a2cac1d95994625
Stefan Kimmer <skimmer@s2e-systems.com>
2023-06-28 16:19:33 +0200

    Add skeleton

---
+++ src/main_publisher.rs
@@ -0,0 +1,110 @@
+use gstreamer::prelude::*;
+
+fn main() {
+    gstreamer::init().unwrap();
+
+
+    use dust_dds::{
+        domain::domain_participant_factory::DomainParticipantFactory,
+        infrastructure::{qos::QosKind, status::NO_STATUS},
+        topic_definition::type_support::{DdsSerde, DdsType},
+    };
+
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Deserialize, Serialize, DdsType, DdsSerde)]
+    struct Video {
+        #[key]
+        userid: i16,
+        frameNum: i32,
+        frame: Vec<u8>,
+    }
+
+    let domain_id = 0;
+    let participant_factory = DomainParticipantFactory::get_instance();
+
+    let participant = participant_factory
+        .create_participant(domain_id, QosKind::Default, None, NO_STATUS)
+        .unwrap();
+
+    let topic = participant
+        .create_topic::<Video>("VideoStream", QosKind::Default, None, NO_STATUS)
+        .unwrap();
+
+    let publisher = participant
+        .create_publisher(QosKind::Default, None, NO_STATUS)
+        .unwrap();
+
+    let writer = publisher
+        .create_datawriter(&topic, QosKind::Default, None, NO_STATUS)
+        .unwrap();
+
+
+    let pipeline = gstreamer::parse_launch(&format!(
+        "videotestsrc horizontal-speed=1 ! video/x-raw,format=I420,width=160,height=90,framerate=10/1 ! appsink name=appsink"
+    ))
+    .unwrap();
+
+    // Start playing
+    pipeline
+        .set_state(gstreamer::State::Playing)
+        .expect("Unable to set the pipeline to the `Playing` state");
+
+    let bin = pipeline.downcast_ref::<gstreamer::Bin>().unwrap();
+    let appsink_element = bin.by_name("appsink").unwrap();
+    let appsink = appsink_element
+        .downcast_ref::<gstreamer_app::AppSink>()
+        .unwrap();
+
+    let mut i = 0;
+    appsink.set_callbacks(
+        gstreamer_app::AppSinkCallbacks::builder()
+            .new_sample(move |s| {
+                if let Ok(sample) = s.pull_sample() {
+                    let b = sample.buffer().unwrap().map_readable().unwrap();
+                    let bytes = b.as_slice();
+
+                    let video_sample = Video {
+                        userid: 8,
+                        frameNum: i,
+                        frame: bytes.to_vec(),
+                    };
+                    writer.write(&video_sample, None).unwrap();
+                    i = i + 1;
+                    println!("Wrote sample {:?}", i);
+
+                    use std::io::{self, Write};
+                    let _ = io::stdout().flush();
+                }
+
+                Ok(gstreamer::FlowSuccess::Ok)
+            })
+            .build(),
+    );
+
+
+    // Wait until error or EOS
+    let bus = pipeline.bus().unwrap();
+    for msg in bus.iter_timed(gstreamer::ClockTime::NONE) {
+        use gstreamer::MessageView;
+
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                println!(
+                    "Error from {:?}: {} ({:?})",
+                    err.src().map(|s| s.path_string()),
+                    err.error(),
+                    err.debug()
+                );
+                break;
+            }
+            _ => (),
+        }
+    }
+
+    // Shutdown pipeline
+    pipeline
+        .set_state(gstreamer::State::Null)
+        .expect("Unable to set the pipeline to the `Null` state");
+}
---
+++ src/main_subscriber.rs
@@ -0,0 +1,136 @@
+use dust_dds::{
+    infrastructure::{
+        qos::DataReaderQos,
+        qos_policy::{ReliabilityQosPolicy, ReliabilityQosPolicyKind},
+        time::{Duration, DurationKind},
+    },
+    subscription::data_reader_listener::DataReaderListener,
+};
+use gstreamer::prelude::*;
+
+use dust_dds::{
+    domain::domain_participant_factory::DomainParticipantFactory,
+    infrastructure::{qos::QosKind, status::NO_STATUS},
+    subscription::sample_info::{ANY_INSTANCE_STATE, ANY_SAMPLE_STATE, ANY_VIEW_STATE},
+    topic_definition::type_support::{DdsSerde, DdsType},
+};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, DdsType, DdsSerde, Debug)]
+struct Video {
+    #[key]
+    userid: i16,
+    frameNum: i32,
+    frame: Vec<u8>,
+}
+struct Listener {
+    appsrc: gstreamer_app::AppSrc,
+}
+
+impl DataReaderListener for Listener {
+    type Foo = Video;
+
+    fn on_data_available(
+        &mut self,
+        the_reader: &dust_dds::subscription::data_reader::DataReader<Self::Foo>,
+    ) {
+        println!("on data available");
+        if let Ok(samples) =
+            the_reader.read(1, ANY_SAMPLE_STATE, ANY_VIEW_STATE, ANY_INSTANCE_STATE)
+        {
+            for sample in samples {
+                let sample_data = sample.data.as_ref().unwrap();
+                println!("sample received: {:?}", sample_data.frameNum);
+
+
+                let mut buffer = gstreamer::Buffer::with_size(sample_data.frame.len()).unwrap();
+                {
+                    let buffer_ref = buffer.get_mut().unwrap();
+                    let mut buffer_samples = buffer_ref.map_writable().unwrap();
+                    buffer_samples.clone_from_slice(sample_data.frame.as_slice());
+                }
+                self.appsrc.push_buffer(buffer).unwrap();
+
+                use std::io::{self, Write};
+                let _ = io::stdout().flush();
+            }
+        }
+    }
+}
+
+fn main() {
+    gstreamer::init().unwrap();
+
+    let domain_id = 0;
+    let participant_factory = DomainParticipantFactory::get_instance();
+
+    let participant = participant_factory
+        .create_participant(domain_id, QosKind::Default, None, NO_STATUS)
+        .unwrap();
+
+    let topic = participant
+        .create_topic::<Video>("VideoStream", QosKind::Default, None, NO_STATUS)
+        .unwrap();
+
+    let subscriber = participant
+        .create_subscriber(QosKind::Default, None, NO_STATUS)
+        .unwrap();
+    let reader_qos = DataReaderQos {
+        reliability: ReliabilityQosPolicy {
+            kind: ReliabilityQosPolicyKind::Reliable,
+            max_blocking_time: DurationKind::Infinite,
+        },
+        ..Default::default()
+    };
+
+    let pipeline = gstreamer::parse_launch(&format!(
+        "appsrc name=appsrc ! video/x-raw,format=I420,width=160,height=90,framerate=10/1 ! autovideosink"
+    ))
+    .unwrap();
+
+    // Start playing
+    pipeline
+        .set_state(gstreamer::State::Playing)
+        .expect("Unable to set the pipeline to the `Playing` state");
+
+    let bin = pipeline.downcast_ref::<gstreamer::Bin>().unwrap();
+    let appsrc_element = bin.by_name("appsrc").unwrap();
+    let appsrc = appsrc_element
+        .downcast::<gstreamer_app::AppSrc>()
+        .unwrap();
+
+    let _reader = subscriber
+        .create_datareader(
+            &topic,
+            QosKind::Default,
+            Some(Box::new(Listener { appsrc })),
+            NO_STATUS,
+        )
+        .unwrap();
+
+    // Wait until error or EOS
+    let bus = pipeline.bus().unwrap();
+    for msg in bus.iter_timed(gstreamer::ClockTime::NONE) {
+        use gstreamer::MessageView;
+
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                println!(
+                    "Error from {:?}: {} ({:?})",
+                    err.src().map(|s| s.path_string()),
+                    err.error(),
+                    err.debug()
+                );
+                break;
+            }
+            _ => (),
+        }
+    }
+
+    // Shutdown pipeline
+    pipeline
+        .set_state(gstreamer::State::Null)
+        .expect("Unable to set the pipeline to the `Null` state");
+}
